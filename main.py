d83acd74a6238677e6c01518a49054998cd2d66e
Rob <govolzgo@gmail.com>
2025-01-11 08:04:49 -0500

    Add description of the new columns.

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ with col1: #open a column
 
 with col2:
     st.title("Rob Ballard")
-    content = """Hello, my name is Rob Ballard.  This is me and my wife in December, 2024.  I am a former Computer Science major and COBOL programmer learning my first new programming language in over 30 years.  I have relished the experience, and honestly had forgotten how much I enjoy coding.  Please look over my portfolio, and if there is something you'd like me to code for you, please send me a message on the contact page.
+    content = """Hello, my name is Rob.  This is me and my wife in December, 2024.  I am a former Computer Science major and COBOL programmer learning my first new programming language in over 30 years.  I have relished the experience, and honestly had forgotten how much I enjoy coding.  Please look over my portfolio, and if there is something you'd like me to code for you, please send me a message on the contact page.
     """
     st.info(content)
+
+#better to use a multi-line string variable, and then apply use that variable in your st.write statement
+#when entering free-form text which may expand to > 1 line.  in this case it didn't, but concept still worksl
+content2 = """
+<b>Below you can find some of the apps I have built in Python.  Feel free to contact me!</b>
+"""
+#add html tags here.  only works on st.write lines
+#when the code is unindented, as it is here, it will stand on its own
+#outside the context of the two columns in page setup.  i kinda guessed that part, but it worked.
+st.write(content2, unsafe_allow_html=True)
+
+
+
